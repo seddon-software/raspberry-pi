0704acbdb862340293b35e466aa6a4671861a2cd
Chris Seddon <seddon-software@keme.co.uk>
2019-01-17 15:34:15 +0000

    minor changes

--- announcer.py
+++ announcer.py
@@ -19,6 +19,7 @@ s.bind(('', 0))
 s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1) #this is a broadcast socket
 #my_ip= gethostbyname(gethostname()) #get our IP. Be careful if you have multiple network interfaces or IPs
 my_ip = get_ip()
+print(f"announcer: {my_ip}")
 
 def broadcastServerAddress():
     while 1:

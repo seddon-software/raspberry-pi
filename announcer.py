2c721e156e1d1e47ce5790662ee0d8afbb9edb5c
Chris Seddon <seddon-software@keme.co.uk>
2019-01-17 15:17:47 +0000

    changed announcer to use correct IP address

--- announcer.py
+++ announcer.py
@@ -2,10 +2,23 @@ from time import sleep
 from socket import socket, AF_INET, SOCK_DGRAM, SOL_SOCKET, SO_BROADCAST, gethostbyname, gethostname
 from constants import MAGIC, PORT
 
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
 s = socket(AF_INET, SOCK_DGRAM) #create UDP socket
 s.bind(('', 0))
 s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1) #this is a broadcast socket
-my_ip= gethostbyname(gethostname()) #get our IP. Be careful if you have multiple network interfaces or IPs
+#my_ip= gethostbyname(gethostname()) #get our IP. Be careful if you have multiple network interfaces or IPs
+my_ip = get_ip()
 
 def broadcastServerAddress():
     while 1:
